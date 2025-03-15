1b0201502279d88225856523221b9b53003751bb
Adalberto-ortega <91564454+Adalberto-ortega@users.noreply.github.com>
2025-03-15 01:38:59 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,134 @@
+(function ($) {
+    "use strict";
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+
+
+    // Team carousel
+    $(".team-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: true,
+        loop: true,
+        center: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+
+
+    
+})(jQuery);
+//caje de comentarios 
+function commentBox(){
+	var name=document.getElementById('name').value;
+	var comment=document.getElementById('comment').value;
+ 
+	if(name =="" || comment ==""){
+		alert("Porfavor introduce la informacion requerida!");
+	}else{
+		var parent=document.createElement('div');
+		var el_name=document.createElement('h5');
+		var el_message=document.createElement('p');
+		var el_line=document.createElement('hr');
+		var txt_name=document.createTextNode(name);
+		var txt_message=document.createTextNode(comment);
+		el_name.appendChild(txt_name);
+		el_message.appendChild(txt_message);
+		el_line.style.border='1px solid #000';
+		parent.appendChild(el_name);
+		parent.appendChild(el_line);
+		parent.appendChild(el_message);
+		parent.setAttribute('class', 'pane');
+		document.getElementById('result').appendChild(parent);
+ 
+		document.getElementById('name').value="";
+		document.getElementById('comment').value="";
+	}
+ 
+}
+
